837e48465c73d8091ffbf5ca18ace564d0e3385c
jiangyuang <jiangyuang1995@gmail.com>
2021-07-12 23:40:46 -0400

    test vgg

--- models/Update.py
+++ models/Update.py
@@ -42,9 +42,9 @@ class LocalUpdate(object):
 
         epoch_loss = []
         for iter in range(self.args.local_ep):
+            print(f"local_iter: {iter}, local ep: {self.args.local_ep}")
             batch_loss = []
             for batch_idx, (images, labels) in enumerate(self.ldr_train):
-                print(batch_idx, len(self.ldr_train))
                 images, labels = images.to(self.args.device), labels.to(self.args.device)
                 net.zero_grad()
                 log_probs = net(images)
